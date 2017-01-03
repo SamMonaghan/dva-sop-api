b652a1b36ca93de386818b1706ad577ad1464dcb
Nick Miller <nick.miller@govlawtech.com.au>
2017-01-04 10:24:04 +1100

    add assert

--- src/test/java/au/gov/dva/dvasopapi/tests/localonly/IntegrationTests.java
+++ src/test/java/au/gov/dva/dvasopapi/tests/localonly/IntegrationTests.java
@@ -9,6 +9,7 @@ import au.gov.dva.sopapi.sopref.data.sops.StoredSop;
 import au.gov.dva.sopapi.sopref.data.updates.SoPLoader;
 import au.gov.dva.sopapi.sopref.parsing.factories.ServiceLocator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -24,7 +25,10 @@ public class IntegrationTests {
         Repository localRepository = new AzureStorageRepository("UseDevelopmentStorage=true");
         SoPLoader underTest = new SoPLoader(localRepository, new FederalRegisterOfLegislation(),s -> ServiceLocator.findTextCleanser(s),s -> ServiceLocator.findSoPFactory(s));
         CompletableFuture<SoP> result = underTest.createGetSopTask("F2014L00933");
+        Assert.assertTrue(result.get() != null);
         SoP sop = result.get();
+
         System.out.println(TestUtils.prettyPrint(StoredSop.toJson(sop)));
+
     }
 }
