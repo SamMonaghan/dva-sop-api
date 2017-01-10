c157949c99034380eb846520d243fed13237c938
Nicholas Miller <nick.miller@govlawtech.com.au>
2017-01-11 09:57:39 +1100

    add received date

--- src/main/java/au/gov/dva/sopapi/interfaces/model/LegislationRegisterEmailUpdate.java
+++ src/main/java/au/gov/dva/sopapi/interfaces/model/LegislationRegisterEmailUpdate.java
@@ -1,10 +1,12 @@
 package au.gov.dva.sopapi.interfaces.model;
 
 import java.net.URL;
+import java.time.OffsetDateTime;
 
 public interface LegislationRegisterEmailUpdate {
     String getInstrumentTitle(); // eg "Amendment Statement of Principles concerning panic disorder (No. 101 of 2016)"
     String getInstrumentDescription(); // eg "This instrument amends the Statement of Principles concerning panic disorder, as determined by the Repatriation Medical Authority."
     String getUpdateDescription(); // eg "Item was amended" or "Some or all of this item commenced on 2/11/2016"
     URL getRegisterLink(); // eg https://www.legislation.gov.au/Details/F2014L01390
+    OffsetDateTime getDateReceived(); // the date this particular update was received via email
 }
