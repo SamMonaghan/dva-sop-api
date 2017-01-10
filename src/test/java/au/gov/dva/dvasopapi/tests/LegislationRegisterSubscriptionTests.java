7136316678eafbea75a07d86ac9927881dfc4502
Nicholas Miller <nick.miller@govlawtech.com.au>
2017-01-11 10:01:52 +1100

    update tests

---
+++ src/test/java/au/gov/dva/dvasopapi/tests/LegislationRegisterSubscriptionTests.java
@@ -0,0 +1,16 @@
+package au.gov.dva.dvasopapi.tests;
+
+import au.gov.dva.dvasopapi.tests.categories.IntegrationTest;
+import au.gov.dva.sopapi.interfaces.model.LegislationRegisterEmailUpdate;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class LegislationRegisterSubscriptionTests {
+
+    @Test
+    @Category(IntegrationTest.class)
+    public void retrieve()
+    {
+        
+    }
+}
