b03168cf3c9b3b6f30ee8a0be015f9df4acdca9f
Tobias Brown <brown.tobias@gmail.com>
2017-01-15 12:44:33 +1100

    Moved osteoarthritis SOPs to correct directories

--- src/test/scala/OsteoarthritisTests.scala
+++ src/test/scala/OsteoarthritisTests.scala
@@ -9,14 +9,14 @@ import org.scalatest.junit.JUnitRunner
 class OsteoarthritisTests extends FunSuite {
 
   test("Parse entire RH LS SoP") {
-    val result = ParserTestUtils.executeWholeParsingPipeline("F2011C00492", "sops_rh/F2011C00492.pdf")
+    val result = ParserTestUtils.executeWholeParsingPipeline("F2011C00491", "sops_rh/F2011C00491.pdf")
     System.out.print(TestUtils.prettyPrint(StoredSop.toJson(result)))
     assert(result != null)
   }
 
   ignore("Parse entire BoP LS SoP")
   {
-    val result = ParserTestUtils.executeWholeParsingPipeline("F2011C00491", "sops_bop/F2011C00491.pdf")
+    val result = ParserTestUtils.executeWholeParsingPipeline("F2011C00492", "sops_bop/F2011C00492.pdf")
     System.out.println(TestUtils.prettyPrint(StoredSop.toJson(result)))
     assert(result != null)
   }
