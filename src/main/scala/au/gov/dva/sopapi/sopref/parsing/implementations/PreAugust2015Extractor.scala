7175079e200234c88b4e4ae58ce7715b06f811ab
Tobias Brown <brown.tobias@gmail.com>
2017-01-18 16:45:41 +1100

    Added pre-Aug 2015 extractor

---
+++ src/main/scala/au/gov/dva/sopapi/sopref/parsing/implementations/PreAugust2015Extractor.scala
@@ -0,0 +1,8 @@
+package au.gov.dva.sopapi.sopref.parsing.implementations
+
+import au.gov.dva.sopapi.sopref.parsing.traits.{PreAugust2015SoPExtractor}
+
+
+class PreAugust2015Extractor extends PreAugust2015SoPExtractor {
+
+}
