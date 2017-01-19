af58a78b490f5b0ca81eeb48742e33f781a3f939
Tobias Brown <brown.tobias@gmail.com>
2017-01-19 17:37:40 +1100

    Added test fixture for lumbar spondylosis

--- src/test/scala/LumbarSpondylosisTests.scala
+++ src/test/scala/LumbarSpondylosisTests.scala
@@ -17,15 +17,23 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class LumbarSpondylosisTests extends FunSuite {
 
+  def rhFixture = new {
+    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00933", "sops_rh/F2014L00933.pdf")
+  }
+
+  def bopFixture = new {
+    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00930", "sops_bop/F2014L00930.pdf")
+  }
+
   test("Parse entire RH LS SoP") {
-      val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00933", "sops_rh/F2014L00933.pdf")
+      val result = rhFixture.result
       System.out.print(TestUtils.prettyPrint(StoredSop.toJson(result)))
       assert(result != null)
   }
 
   test("Parse entire BoP LS SoP")
   {
-    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00930", "sops_bop/F2014L00930.pdf")
+    val result = bopFixture.result
     System.out.println(TestUtils.prettyPrint(StoredSop.toJson(result)))
     assert(result != null)
   }
